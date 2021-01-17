d3f4753240b5646dc0c1e989bcf09d7c35037f91
Oscar García <oscar.github@linaresdigital.com>
2021-01-17 22:56:41 +0000

    Corregida errata con URL de retorno

--- inicio.sh
+++ inicio.sh
@@ -2,5 +2,5 @@
 
 cat <<EOF > public/config.js
 const idToken = "$TWITCH_CLIENTID";
-const redirect = "${GITPOD_WORKSPACE_URL/:\/\//:\/\/8001-}";
+const redirect = "${GITPOD_WORKSPACE_URL/:\/\//:\/\/8001-}/callback.html";
 EOF
