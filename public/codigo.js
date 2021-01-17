2c77f37bca789ed7411f86be6651353b47cf5a0c
Oscar García <oscar.github@linaresdigital.com>
2021-01-17 22:57:56 +0000

    Antes de usar "win" comprobamos su valor

--- public/codigo.js
+++ public/codigo.js
@@ -1,9 +1,12 @@
-
+/* Funcionalidad de comunicación con la ventana de inicio de sesión */
 let token = false;
+let win = false;
 window.addEventListener("message", evento => {
     if (evento.origin == location.origin) {
         /* Cerramos la ventana de inicio de sesión */
-        this.win.close();
+        if (win !== false) {
+            win.close();
+        }
         /* Guardamos las credenciales */
         token = evento.data.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1]
         localStorage.setItem('token', token);
@@ -11,19 +14,16 @@ window.addEventListener("message", evento => {
     }
 }, false);
 
-
-
+/* Preparamos la funcionalidad de inicio de sesión */
 const boton_login = document.getElementById("login");
 
-const idToken = "q5gn79f80294bvyb3fwwfb11uhe23w";
-const redirect = "https://8001-f01d5132-7a01-4c8c-883b-11bdc7049c95.ws-eu03.gitpod.io/callback.html";
 const scope = "openid channel_read";
 let aleatorio = Math.random(0).toString(36).substr(2);;
 console.log("Aleatorio: ", aleatorio);
 
 boton_login.addEventListener("click", evento => {
     win = window.open(
-        `https://id.twitch.tv/oauth2/authorize?client_id=${idToken}&redirect_uri=${redirect}&response_type=token&scope=${scope}&state=${aleatorio}`,
+        `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirect}&response_type=token&scope=${scope}&state=${aleatorio}`,
         'twitchLogin',
         "location=yes,height=620,width=520,scrollbars=no,resizable=no,status=yes"
     );
